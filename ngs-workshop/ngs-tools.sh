f2c9096f582ceed04389dcd081765a1843c05261
Jerico Revote <jerico.revote@gmail.com>
2014-11-04 14:41:02 +1100

    Added additional R libraries to be installed on the image. Fixed configuration/compile issue with Cufflinks.

--- ngs-workshop/ngs-tools.sh
+++ ngs-workshop/ngs-tools.sh
@@ -7,71 +7,50 @@ owner='root'
 ## Prepare ##
 #############
 apt-get update
+packages=()
 ####################
 
 
 ###################
 ## Miscellaneous ##
 ###################
-apt-get install -y wget 
-apt-get install -y curl 
-apt-get install -y vim 
-apt-get install -y screen
-apt-get install -y zip
-apt-get install -y unzip
+packages=(${packages[*]} wget curl vim screen zip unzip)
 ####################
 
 
 ###############
 ## Compilers ##
 ###############
-apt-get install -y gcc
-apt-get install -y g++ 
-apt-get install -y gfortran
-apt-get install -y cmake
-apt-get install -y cmake-curses-gui
+packages=(${packages[*]} gcc g++ gfortran cmake cmake-curses-gui)
 ####################
 
 
 ##########################
 ## Development Packages ##
 ##########################
-apt-get install -y libgtextutils-dev
-apt-get install -y libgtextutils0
-apt-get install -y texlive-fonts-recommended
-apt-get install -y libgd-barcode-perl
-apt-get install -y libgd-graph-perl
-apt-get install -y libgd-graph3d-perl
-apt-get install -y libstatistics-descriptive-perl
-apt-get install -y libdbi-perl
-apt-get install -y xorg-dev
-apt-get install -y texinfo
-apt-get install -y build-essential
-apt-get install -y libpng12-0-dev
-apt-get install -y libmysqlclient-dev
-apt-get install -y libboost-dev
-apt-get install -y libboost-doc
-apt-get install -y libboost-thread-dev
-apt-get install -y libboost-random-dev
-apt-get install -y libssl-dev
-apt-get install -y zlib1g-dev
-apt-get install -y ncurses-dev
-apt-get install -y xorg-dev
-apt-get install -y libreadline6-dev
-apt-get install -y texlive-latex-base
-apt-get install -y texlive-fonts-extra
-apt-get install -y libfontconfig1-dev
-apt-get install -y libfreetype6-dev
-apt-get install -y libx11-dev
-apt-get install -y libxcursor-dev
-apt-get install -y libxext-dev
-apt-get install -y libxft-dev
-apt-get install -y libxi-dev
-apt-get install -y libxrandr-dev
-apt-get install -y libxrender-dev
-apt-get install -y libqt4-dev
-apt-get install -y python-software-properties
-apt-get install -y libxml2-dev
+packages=(${packages[*]} libgtextutils-dev libgtextutils0 
+texlive-fonts-recommended libgd-barcode-perl libgd-graph-perl 
+libgd-graph3d-perl libstatistics-descriptive-perl libdbi-perl 
+xorg-dev texinfo build-essential libpng12-0-dev libmysqlclient-dev
+libboost1.48-all-dev libssl-dev zlib1g-dev ncurses-dev xorg-dev
+libreadline6-dev texlive-latex-base texlive-fonts-extra libfontconfig1-dev 
+libfreetype6-dev libx11-dev libxcursor-dev libxext-dev libxft-dev
+libxi-dev libxrandr-dev libxrender-dev libqt4-dev python-software-properties
+libxml2-dev libeigen3-dev libeigen3-doc libcurl4-openssl-dev autoconf)
+####################
+
+
+#####################
+## Package Install ##
+#####################
+apt-get install -y ${packages[@]}
+####################
+
+
+################
+## Pre Flight ##
+################
+cp -r /usr/include/eigen3/Eigen /usr/include/
 ####################
 
 
@@ -91,8 +70,9 @@ apt-get install -y oracle-java8-installer
 ############
 cd /tmp/
 ubuntu_version=`lsb_release -s -c`
-wget --no-check-certificate https://apt.puppetlabs.com/puppetlabs-release-$ubuntu_version.deb
+wget -4 --no-check-certificate https://apt.puppetlabs.com/puppetlabs-release-$ubuntu_version.deb
 dpkg -i puppetlabs-release-$ubuntu_version.deb
+apt-get update
 apt-get install puppet
 rm puppetlabs-release-$ubuntu_version.deb
 ####################
@@ -103,7 +83,7 @@ rm puppetlabs-release-$ubuntu_version.deb
 ###############
 cd /tmp/
 # Download DEB package
-wget --no-check-certificate https://launchpad.net/~tbooth/+archive/ppa1/+files/gnx-tools_0.1%2B20120305-1_all.deb
+wget -4 --no-check-certificate https://launchpad.net/~tbooth/+archive/ppa1/+files/gnx-tools_0.1%2B20120305-1_all.deb
 dpkg -i gnx-tools_0.1+20120305-1_all.deb
 # Cleanup
 rm gnx-tools_0.1+20120305-1_all.deb
@@ -122,10 +102,10 @@ else
 fi
 # Download the jar files
 cd $install_dir/$tool_name
-wget --no-check-certificate http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.1.zip
+wget -4 --no-check-certificate http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.1.zip
 unzip fastqc_v0.11.1.zip
 mv FastQC 0.11.1
-ln -s $install_dir/$tool_name/0.11.1 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/0.11.1 $install_dir/$tool_name/fastqc-default
 # Cleanup
 rm fastqc_v0.11.1.zip
 ####################
@@ -143,13 +123,13 @@ else
 fi
 # Download the source code
 cd $install_dir/$tool_name
-wget --no-check-certificate https://github.com/agordon/fastx_toolkit/releases/download/0.0.14/fastx_toolkit-0.0.14.tar.bz2
+wget -4 --no-check-certificate https://github.com/agordon/fastx_toolkit/releases/download/0.0.14/fastx_toolkit-0.0.14.tar.bz2
 tar -xjf fastx_toolkit-0.0.14.tar.bz2
 # Compile
 cd fastx_toolkit-0.0.14
 ./configure --prefix=$install_dir/$tool_name/0.0.14
 make; make install
-ln -s $install_dir/$tool_name/0.0.14 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/0.0.14 $install_dir/$tool_name/fastx-toolkit-default
 # Cleanup
 cd ../
 rm -r fastx_toolkit-0.0.14
@@ -169,10 +149,10 @@ else
 fi
 # Download the jar files
 cd $install_dir/$tool_name
-wget --no-check-certificate http://sourceforge.net/projects/picard/files/picard-tools/1.114/picard-tools-1.114.zip
+wget -4 --no-check-certificate http://sourceforge.net/projects/picard/files/picard-tools/1.114/picard-tools-1.114.zip
 unzip picard-tools-1.114.zip
 mv picard-tools-1.114 1.114
-ln -s $install_dir/$tool_name/1.114 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/1.114 $install_dir/$tool_name/picard-default
 # Cleanup
 rm picard-tools-1.114.zip
 ####################
@@ -190,13 +170,13 @@ else
 fi
 # Download the source code
 cd $install_dir/$tool_name
-wget --no-check-certificate http://sourceforge.net/projects/bowtie-bio/files/bowtie/1.0.1/bowtie-1.0.1-src.zip
+wget -4 --no-check-certificate http://sourceforge.net/projects/bowtie-bio/files/bowtie/1.0.1/bowtie-1.0.1-src.zip
 unzip bowtie-1.0.1-src.zip
 # Compile
 mv bowtie-1.0.1 1.0.1
 cd 1.0.1/
 make
-ln -s $install_dir/$tool_name/1.0.1 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/1.0.1 $install_dir/$tool_name/bowtie-default
 # Cleanup
 cd ../
 rm bowtie-1.0.1-src.zip
@@ -206,7 +186,7 @@ rm bowtie-1.0.1-src.zip
 ##############
 ## Bowtie 2 ##
 ##############
-tool_name='Bowtie2'
+tool_name='Bowtie'
 if [ ! -e "$install_dir/$tool_name" ]; then
   echo "Creating installation directory for $tool_name"
   mkdir -p "$install_dir/$tool_name"
@@ -215,13 +195,13 @@ else
 fi
 # Download the source code
 cd $install_dir/$tool_name
-wget --no-check-certificate http://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.2.3/bowtie2-2.2.3-source.zip
+wget -4 --no-check-certificate http://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.2.3/bowtie2-2.2.3-source.zip
 unzip bowtie2-2.2.3-source.zip
 # Compile
 mv bowtie2-2.2.3 2.2.3
 cd 2.2.3/
 make
-ln -s $install_dir/$tool_name/2.2.3 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/2.2.3 $install_dir/$tool_name/bowtie2-default
 # Cleanup
 cd ../
 rm bowtie2-2.2.3-source.zip
@@ -240,7 +220,7 @@ else
 fi
 # Download the source code
 cd $install_dir/$tool_name
-wget --no-check-certificate http://sourceforge.net/projects/samtools/files/samtools/0.1.18/samtools-0.1.18.tar.bz2
+wget -4 --no-check-certificate http://sourceforge.net/projects/samtools/files/samtools/0.1.18/samtools-0.1.18.tar.bz2
 tar -xjf samtools-0.1.18.tar.bz2
 # Compile
 mv samtools-0.1.18 0.1.18
@@ -250,7 +230,7 @@ mkdir -p $install_dir/$tool_name/0.1.18/lib
 cp $install_dir/$tool_name/0.1.18/libbam.a $install_dir/$tool_name/0.1.18/lib/
 mkdir -p $install_dir/$tool_name/0.1.18/include/bam
 cp $install_dir/$tool_name/0.1.18/*.h $install_dir/$tool_name/0.1.18/include/bam/
-ln -s $install_dir/$tool_name/0.1.18 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/0.1.18 $install_dir/$tool_name/samtools-default
 # Cleanup
 cd ../
 rm samtools-0.1.18.tar.bz2
@@ -269,13 +249,13 @@ else
 fi
 # Download the source code
 cd $install_dir/$tool_name
-wget --no-check-certificate https://github.com/arq5x/bedtools2/releases/download/v2.20.1/bedtools-2.20.1.tar.gz
+wget -4 --no-check-certificate https://github.com/arq5x/bedtools2/releases/download/v2.20.1/bedtools-2.20.1.tar.gz
 tar -xzf bedtools-2.20.1.tar.gz
 # Compile
 mv bedtools2-2.20.1 2.20.1
 cd 2.20.1
 make
-ln -s $install_dir/$tool_name/2.20.1 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/2.20.1 $install_dir/$tool_name/bedtools-default
 # Cleanup
 cd ../
 rm bedtools-2.20.1.tar.gz
@@ -294,12 +274,12 @@ else
 fi
 # Download the source code
 cd $install_dir/$tool_name
-wget --no-check-certificate http://hgdownload.cse.ucsc.edu/admin/exe/userApps.src.tgz
+wget -4 --no-check-certificate http://hgdownload.cse.ucsc.edu/admin/exe/userApps.src.tgz
 tar -xzf userApps.src.tgz
 # Compile
 cd userApps/
 make
-ln -s $install_dir/$tool_name/userApps $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/userApps $install_dir/$tool_name/ucsctools-default
 # Cleanup
 cd ../
 rm userApps.src.tgz
@@ -318,7 +298,7 @@ else
 fi
 # Download the source code
 cd $install_dir/$tool_name
-wget --no-check-certificate https://www.ebi.ac.uk/~zerbino/velvet/velvet_1.2.10.tgz
+wget -4 --no-check-certificate https://www.ebi.ac.uk/~zerbino/velvet/velvet_1.2.10.tgz
 tar -xzf velvet_1.2.10.tgz
 # Compile
 mv velvet_1.2.10 1.2.10
@@ -326,7 +306,7 @@ cd 1.2.10
 make 'MAXKMERLENGTH=59' 'LONGSEQUENCES=1' 'OPENMP=1'
 make 'MAXKMERLENGTH=59' 'LONGSEQUENCES=1' 'OPENMP=1' color
 make doc
-ln -s $install_dir/$tool_name/1.2.10 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/1.2.10 $install_dir/$tool_name/velvet-default
 # Cleanup
 cd ../
 rm velvet_1.2.10.tgz
@@ -345,16 +325,16 @@ else
 fi
 # Download the source code
 cd $install_dir/$tool_name
-wget --no-check-certificate https://github.com/taoliu/MACS/archive/v1.4.2.tar.gz
+wget -4 --no-check-certificate https://github.com/taoliu/MACS/archive/v1.4.2.tar.gz
 tar -xzf v1.4.2.tar.gz
 # Compile
 cd MACS-1.4.2/
 python setup.py install --prefix $install_dir/$tool_name/1.4.2
-ln -s $install_dir/$tool_name/1.4.2 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/1.4.2 $install_dir/$tool_name/macs-default
 # Cleanup
 cd ../
 rm -r MACS-1.4.2
-rm MACS-1.4.2-1.tar.gz
+rm v1.4.2.tar.gz
 ####################
 
 
@@ -370,10 +350,10 @@ else
 fi
 # Download the jar files
 cd $install_dir/$tool_name
-wget --no-check-certificate http://www.ebi.ac.uk/sites/ebi.ac.uk/files/groups/bertone/software/PeakAnalyzer_1.4.tar.gz
+wget -4 --no-check-certificate http://www.ebi.ac.uk/sites/ebi.ac.uk/files/groups/bertone/software/PeakAnalyzer_1.4.tar.gz
 tar -xzf PeakAnalyzer_1.4.tar.gz
 mv PeakAnalyzer_1.4 1.4
-ln -s $install_dir/$tool_name/1.4 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/1.4 $install_dir/$tool_name/peakanalyzer-default
 # Cleanup
 rm PeakAnalyzer_1.4.tar.gz
 ####################
@@ -391,14 +371,14 @@ else
 fi
 # Download the jar files
 cd $install_dir/$tool_name
-wget --no-check-certificate http://ccb.jhu.edu/software/tophat/downloads/tophat-1.4.1.tar.gz
+wget -4 --no-check-certificate http://ccb.jhu.edu/software/tophat/downloads/tophat-1.4.1.tar.gz
 tar -xzf tophat-1.4.1.tar.gz
 # Compile
 cd tophat-1.4.1
-./configure --with-bam=$install_dir/SAMtools/default --prefix=$install_dir/$tool_name/1.4.1
+./configure --prefix=$install_dir/$tool_name/1.4.1 --with-bam=$install_dir/SAMtools/samtools-default 
 make all
 make install
-ln -s $install_dir/$tool_name/1.4.1 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/1.4.1 $install_dir/$tool_name/tophat-default
 # Cleanup
 cd ../
 rm -r tophat-1.4.1
@@ -418,14 +398,14 @@ else
 fi
 # Download the source code
 cd $install_dir/$tool_name
-wget --no-check-certificate http://cufflinks.cbcb.umd.edu/downloads/cufflinks-2.2.1.tar.gz
+wget -4 --no-check-certificate http://cufflinks.cbcb.umd.edu/downloads/cufflinks-2.2.1.tar.gz
 tar -xzf cufflinks-2.2.1.tar.gz
 # Compile
 cd cufflinks-2.2.1/
-./configure --prefix=/tools/Cufflinks/2.2.1 --with-bam=/tools/SAMtools/0.1.18/ --with-boost=/usr/
+./configure --prefix=/tools/Cufflinks/2.2.1 --with-bam=/tools/SAMtools/samtools-default --with-boost=/usr/
 make all
 make install
-ln -s $install_dir/$tool_name/2.2.1 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/2.2.1 $install_dir/$tool_name/cufflinks-default
 # Cleanup
 cd ../
 rm -r cufflinks-2.2.1/
@@ -445,10 +425,10 @@ else
 fi
 # Download the jar files
 cd $install_dir/$tool_name
-wget --no-check-certificate http://www.broadinstitute.org/igv/projects/downloads/IGV_2.3.32.zip
+wget -4 --no-check-certificate http://www.broadinstitute.org/igv/projects/downloads/IGV_2.3.32.zip
 unzip IGV_2.3.32.zip
 mv IGV_2.3.32 2.3.32
-ln -s $install_dir/$tool_name/2.3.32 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/2.3.32 $install_dir/$tool_name/igv-default
 # Cleanup
 rm IGV_2.3.32.zip
 ####################
@@ -466,9 +446,9 @@ else
 fi
 # Download the jar files
 cd $install_dir/$tool_name
-wget --no-check-certificate http://www.broadinstitute.org/igv/projects/downloads/igvtools_2.3.32.zip
+wget -4 --no-check-certificate http://www.broadinstitute.org/igv/projects/downloads/igvtools_2.3.32.zip
 unzip igvtools_2.3.32.zip
-ln -s $install_dir/$tool_name/2.3.32 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/2.3.32 $install_dir/$tool_name/igvtools-default
 mv IGVTools 2.3.32
 # Cleanup
 rm igvtools_2.3.32.zip
@@ -487,14 +467,23 @@ else
 fi
 # Download the source code
 cd $install_dir/$tool_name
-wget --no-check-certificate http://cran.csiro.au/src/base/R-3/R-3.1.0.tar.gz
+wget -4 --no-check-certificate http://cran.csiro.au/src/base/R-3/R-3.1.0.tar.gz
 tar -xzf R-3.1.0.tar.gz
 # Compile
 cd R-3.1.0/
 ./configure --prefix=/tools/R/3.1.0
 make all
 make install
-ln -s $install_dir/$tool_name/3.1.0 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/3.1.0 $install_dir/$tool_name/r-default
+# Additional R Libraries
+cat > additional.R << EOF
+source("http://bioconductor.org/biocLite.R")
+biocLite("DESeq2")
+biocLite("edgeR")
+biocLite("cummeRbund")
+install.packages(pkgs="gplots",repos="http://cran.csiro.au/")
+EOF
+/tools/R/r-default/bin/R CMD BATCH additional.R
 # Cleanup
 cd ../
 rm -r R-3.1.0
@@ -514,13 +503,13 @@ else
 fi
 # Download the source code
 cd $install_dir/$tool_name
-wget --no-check-certificate http://sourceforge.net/projects/amos/files/amos/3.1.0/amos-3.1.0.tar.gz
+wget -4 --no-check-certificate http://sourceforge.net/projects/amos/files/amos/3.1.0/amos-3.1.0.tar.gz
 tar -xzf amos-3.1.0.tar.gz
 cd amos-3.1.0/
 ./configure --prefix=$install_dir/$tool_name/3.1.0 --enable-minimus=no
 make
 make install
-ln -s $install_dir/$tool_name/3.1.0 $install_dir/$tool_name/default
+ln -s $install_dir/$tool_name/3.1.0 $install_dir/$tool_name/amos-default
 # Cleanup
 cd ../
 rm -r amos-3.1.0/
@@ -536,25 +525,25 @@ echo "" >> /etc/bash.bashrc
 echo "# BPA CSIRO NGS Tools" >> /etc/bash.bashrc
 echo "alias gnx=/usr/bin/gnx-tools" >> /etc/bash.bashrc
 echo "export LD_LIBRARY_PATH=/tools/Qt/qt-default/lib:\$LD_LIBRARY_PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/FastQC/default:\$PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/FASTX-Toolkit/default/bin:\$PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/Picard/default:\$PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/Bowtie/default:\$PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/Bowtie2/default:\$PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/SAMtools/default:\$PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/SAMtools/default/bcftools:\$PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/SAMtools/default/misc:\$PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/bedtools/default/bin:\$PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/UCSCtools/userApps/bin:\$PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/IGV/default:\$PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/IGVTools/default:\$PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/MACS/default/bin:\$PATH" >> /etc/bash.bashrc
-echo "alias macs=/tools/MACS/default/bin/macs14" >> /etc/bash.bashrc
-echo "export PYTHONPATH=/tools/MACS/default/lib/python2.7/site-packages:\$PYTHONPATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/TopHat/default/bin:\$PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/Cufflinks/default/bin:\$PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/Velvet/default:\$PATH" >> /etc/bash.bashrc
-echo "alias velveth_long=/tools/Velvet/default/velveth" >> /etc/bash.bashrc
-echo "alias velvetg_long=/tools/Velvet/default/velvetg" >> /etc/bash.bashrc
-echo "export PATH=/tools/AMOS/default/bin:\$PATH" >> /etc/bash.bashrc
-echo "export PATH=/tools/R/default/bin:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/FastQC/fastqc-default:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/FASTX-Toolkit/fastx-toolkit-default/bin:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/Picard/picard-default:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/Bowtie/bowtie-default:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/Bowtie/bowtie2-default:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/SAMtools/samtools-default:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/SAMtools/samtools-default/bcftools:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/SAMtools/samtools-default/misc:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/bedtools/bedtools-default/bin:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/UCSCtools/ucsctools-default/bin:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/IGV/igv-default:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/IGVTools/igvtools-default:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/MACS/macs-default/bin:\$PATH" >> /etc/bash.bashrc
+echo "alias macs=/tools/MACS/macs-default/bin/macs14" >> /etc/bash.bashrc
+echo "export PYTHONPATH=/tools/MACS/macs-default/lib/python2.7/site-packages:\$PYTHONPATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/TopHat/tophat-default/bin:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/Cufflinks/cufflinks-default/bin:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/Velvet/velvet-default:\$PATH" >> /etc/bash.bashrc
+echo "alias velveth_long=/tools/Velvet/velvet-default/velveth" >> /etc/bash.bashrc
+echo "alias velvetg_long=/tools/Velvet/velvet-default/velvetg" >> /etc/bash.bashrc
+echo "export PATH=/tools/AMOS/amos-default/bin:\$PATH" >> /etc/bash.bashrc
+echo "export PATH=/tools/R/r-default/bin:\$PATH" >> /etc/bash.bashrc
